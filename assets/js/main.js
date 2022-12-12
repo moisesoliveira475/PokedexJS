40f91d5539b624d1a553d186e5bd1276ee3e5a40
Moisés Santos <moises475oliveira@gmail.com>
2022-12-12 15:35:01 -0300

    pokeapi connect

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+fetch(url)
+    .then(function(response) {
+        response
+            .json()
+            .then(function(responseBody){
+                console.log(responseBody)
+            })
+    })
+    .catch(function(error) {
+        console.error(error)
+    })
+    .finally(function() {
+        console.log('requisição concluída')
+    })
+
+const x = 10 + 10;
+console.log(x);
